49221ef6fe8de23ca44fcff2e3b75399459872f7
Volodymyr Voronov <malones.vladimir@gmail.com>
2023-07-16 18:29:27 +0200

    Updated TodayHeader.

--- src/components/TodayHeader/TodayHeader.tsx
+++ src/components/TodayHeader/TodayHeader.tsx
@@ -10,6 +10,18 @@ interface ITodayHeaderProps extends ComponentProps<'div'> {
   className?: string;
 }
 
+const variants = {
+  initial: { opacity: 0, filter: 'blur(10px)' },
+  exit: { opacity: 0, filter: 'blur(10px)', transition: { duration: 0.5 } },
+  animate: { opacity: 1, filter: 'blur(0px)', transition: { duration: 1 } },
+};
+
+const animations = {
+  initial: 'initial',
+  exit: 'exit',
+  animate: 'animate',
+};
+
 const TodayHeader = ({
   className,
 
@@ -25,52 +37,38 @@ const TodayHeader = ({
     { month: 'long' },
   );
 
-  const variants = {
-    initial: { opacity: 0, filter: 'blur(10px)' },
-    exit: { opacity: 0, filter: 'blur(10px)', transition: { duration: 0.5 } },
-    animate: { opacity: 1, filter: 'blur(0px)', transition: { duration: 1 } },
-  };
-
   return (
     <div className={cn(styles.root, className)} {...props}>
       <AnimatePresence mode='sync'>
         <motion.span
+          className={styles.day}
           key={day + 100}
           variants={variants}
-          initial='initial'
-          exit='exit'
-          animate='animate'
-          className={styles.day}
+          {...animations}
         >
           {day}
         </motion.span>
         <motion.span
+          className={styles['week-day']}
           key={day + 200}
           variants={variants}
-          initial='initial'
-          exit='exit'
-          animate='animate'
-          className={styles['week-day']}
+          {...animations}
         >
           {dayOfTheWeek}
         </motion.span>
         <motion.span
+          className={styles.month}
           key={day + 300}
           variants={variants}
-          initial='initial'
-          exit='exit'
-          animate='animate'
-          className={styles.month}
+          {...animations}
         >
           {monthOfTheYear}
         </motion.span>
         <motion.span
+          className={styles.year}
           key={day + 400}
           variants={variants}
-          initial='initial'
-          exit='exit'
-          animate='animate'
-          className={styles.year}
+          {...animations}
         >
           {year}
         </motion.span>
